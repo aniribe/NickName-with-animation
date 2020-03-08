51511d8283cea005c8ae5c15399e18ac72cfc1d1
Irina Belousova <irinabelousova11@gmail.com>
2020-03-08 22:06:32 +0400

    Final version

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
-let ball = document.getElementById("ball");
+// let ball = document.getElementById("ball");
 let particles = document.getElementById("particles");
 let btn = document.getElementById("btn");
 let supportedFlag = $.keyframe.isSupported();
 let ballContainer = document.getElementById("ball-container");
+let letters = document.getElementById("letters");
 
-for (let i = 0; i < 35; i++) {
-  let particle = document.createElement("span");
-  particle.classList.add("particle");
-
-  particles.appendChild(particle);
-}
+addSpansforParticles();
 
 btn.addEventListener("click", function() {
+  //Add or remove class Active to the elements
   particles.classList.toggle("active");
   btn.classList.toggle("active");
   ballContainer.classList.toggle("active");
 
+  //Add keyFramws dinamically
   if (particles.className === "particles active") {
     for (let i = 0; i < 35; i++) {
-      let left = Math.random(500) * (250 - -250) - 250;
-      let top = Math.random(500) * (250 - -250) - 250;
       let selector = ".particles.active .particle:nth-child(" + (i + 1) + ")";
 
-      $(selector).playKeyframe({
-        name: "particle" + i,
-        duration: "1.5s",
-        delay: i / 40 + "s"
-      });
-
-      $.keyframe.define([
-        {
-          name: "particle" + i,
-          from: { transform: "scale(1)" },
-          to: {
-            left: left + "px",
-            top: top + "px",
-            transform: "scale(0)",
-            visibility: "hidden"
-          }
-        }
-      ]);
+      playAnimation(selector, i);
+      addKeyFrames(i);
     }
+
+    setTimeout(function() {
+      letters.classList.add("active");
+    }, 1500);
+  } else {
+    letters.classList.toggle("active", false);
   }
 });
+
+function addSpansforParticles() {
+  for (let i = 0; i < 35; i++) {
+    let particle = document.createElement("span");
+    particle.classList.add("particle");
+
+    particles.appendChild(particle);
+  }
+}
+
+function playAnimation(elementName, num) {
+  $(elementName).playKeyframe({
+    name: "particle" + num,
+    duration: "1.5s",
+    delay: num / 40 + "s"
+  });
+}
+
+function addKeyFrames(num) {
+  let left = randomNum();
+  let top = randomNum();
+
+  $.keyframe.define([
+    {
+      name: "particle" + num,
+      from: { transform: "scale(1)" },
+      to: {
+        left: left + "px",
+        top: top + "px",
+        transform: "scale(0)",
+        visibility: "hidden"
+      }
+    }
+  ]);
+}
+
+function randomNum() {
+  return Math.random(500) * (250 - -250) - 250;
+}
